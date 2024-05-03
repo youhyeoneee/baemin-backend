a04a9450a1d6c8b75c46c9f1aa715dad318c896d
youhyeoneee <khukyh2018@khu.ac.kr>
2024-05-03 15:41:20 +0900

    init: project

---
+++ src/test/java/com/example/baemin/BaeminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baemin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
